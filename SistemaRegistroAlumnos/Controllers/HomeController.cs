dac1b33893d9f77bc84b99b6f10e6c64d8c3fcf8
MARALY GUADALUPE GUTIERREZ SOLIS <mgomez@ZGNE.NET>
2025-10-22 17:21:36 -0700

    estructura unicial de la app web

---
+++ SistemaRegistroAlumnos/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using SistemaRegistroAlumnos.Models;
+using System.Diagnostics;
+
+namespace SistemaRegistroAlumnos.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult info()
+        {
+            return View();
+        }
+
+        public IActionResult ExportarLista()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
