ffcfd62fd2b3c8faa8aceac23f099b89050fb38d
Pablo Herrera <105743191+Pablenki@users.noreply.github.com>
2025-10-23 21:43:43 -0700

    Versión 23 de octubre

---
+++ SistemaRegistroAlumnos/Controllers/AlumnosController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaRegistroAlumnos.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        // GET: AlumnosController1
+        public ActionResult BuscarAlumnos()
+        {
+            return View();
+        }
+
+        public ActionResult RegistroAlumno()
+        {
+            return View();
+        }
+    }
+}
---
+++ SistemaRegistroAlumnos/Controllers/GraficasController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaRegistroAlumnos.Controllers
+{
+    public class GraficasController : Controller
+    {
+        // GET: GraficasController1
+        public ActionResult G_Pastel()
+        {
+            return View();
+        }
+
+        public ActionResult G_Histograma()
+        {
+            return View();
+        }
+
+        public ActionResult G_Dispersion()
+        {
+            return View();
+        }
+
+        public ActionResult G_Pareto()
+        {
+            return View();
+        }
+
+    }
+}
